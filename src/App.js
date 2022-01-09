5f2732d80d8f9f4aad284342d2c18ba0bdd6d9d5
PavelContraMundum <pavel.sticka@gmail.com>
2022-01-09 09:06:59 +0100

    input field

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackData from './data/FeedbackData'
-
+import FeedbackForm from "./components/FeedbackForm"
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
     const deleteFeedback = (id) => {
-        if (window.confirm('Are you sure you want oto delete?'))
+        if (window.confirm('Are you sure you want to delete?'))
             setFeedback(feedback.filter((item) => item.id !== id))
     }
 
@@ -17,6 +17,7 @@ function App() {
         <>
             <Header />
             <div className="container">
+                <FeedbackForm />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback}
                     handleDelete={deleteFeedback} />
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types'
 
-function FeedbackStats({feedback}) {
+function FeedbackStats({ feedback }) {
     //Calculate ratings avg
     let average = feedback.reduce((acc, cur) => {
-            return acc + cur.rating
+        return acc + cur.rating
     }, 0) / feedback.length
 
     average = average.toFixed(1).replace(/[.,]0$/, '')
